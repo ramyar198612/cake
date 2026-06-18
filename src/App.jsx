1ca285d8f07a9d58f7a86afc82e5619ccb15b257
ramyar198612 <ramyar198612@gmail.com>
2026-06-18 16:39:50 +0530

    Configure GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Cake from './pages/Cake';
